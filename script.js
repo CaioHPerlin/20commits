152856c9de4f3a59b10f7443c2d785365dc3846e
caiohperlin <caiohperlin@gmail.com>
2025-09-09 18:24:56 -0400

    feat: add css styling and basic interactio with js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script básico para o projeto 20 Commits
+console.log("Bem-vindo ao projeto 20 Commits!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  header.addEventListener("click", () => {
+    alert("Você clicou no cabeçalho!");
+  });
+});
